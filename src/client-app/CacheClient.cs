5300f566b4401be6c7ffd5033cad8360ca114d44
Onur Aykac <onuraykac@gmail.com>
2018-01-31 20:52:28 +0300

    Made hc client singleton

---
+++ src/client-app/CacheClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Hazelcast.Client;
+using Hazelcast.Config;
+using Hazelcast.Core;
+
+namespace ClientApplication
+{
+    public class CacheClient
+    {
+        private static readonly Lazy<IHazelcastInstance> Lazy =
+            new Lazy<IHazelcastInstance>(() =>
+            {
+                Console.WriteLine("Created client");
+                
+                var clientConfig = new ClientConfig();
+                clientConfig
+                    .GetNetworkConfig()
+                    .AddAddress("0.0.0.0:5705"
+                        , "0.0.0.0:5706");
+                //.AddAddress("0.0.0.0:5705");
+                clientConfig.SetGroupConfig(new GroupConfig("hc-farm", "s3crEt"));
+                return HazelcastClient.NewHazelcastClient(clientConfig);
+            });
+        
+        public static IHazelcastInstance Instance => Lazy.Value;
+    }
+}
--- src/client-app/Controllers/ValuesController.cs
+++ src/client-app/Controllers/ValuesController.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Hazelcast.Client;
-using Hazelcast.Config;
 using Hazelcast.Core;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,24 +15,14 @@ namespace ClientApplication.Controllers
         [HttpGet("{key}")]
         public string Get(string key)
         {
-            var clientConfig = new ClientConfig();
-            clientConfig
-                .GetNetworkConfig()
-                .AddAddress("0.0.0.0:5705"
-                    , "0.0.0.0:5706");
-            //.AddAddress("0.0.0.0:5705");
-            clientConfig.SetGroupConfig(new GroupConfig("hc-farm", "s3crEt"));
-
-            //client with custom configuration
-            Console.WriteLine(DateTime.Now.ToString() + ": GET > Connecting Hazelcast");
-            IHazelcastInstance client = HazelcastClient.NewHazelcastClient(clientConfig);
-            Console.WriteLine(DateTime.Now.ToString() + ": GET > Connected " + client.GetName());
+            IHazelcastInstance client = CacheClient.Instance;
+            Console.WriteLine(DateTime.Now + ": GET > Connected " + client.GetName());
 
             var map = client.GetMap<string, string>("mymap");
 
-            Console.WriteLine(DateTime.Now.ToString() + ": GET > Retrieving value");
+            Console.WriteLine(DateTime.Now + ": GET > Retrieving value");
             var result = map.Get(key);
-            Console.WriteLine(DateTime.Now.ToString() + ": GET > Retrieved value");
+            Console.WriteLine(DateTime.Now + ": GET > Retrieved value");
 
             return result;
         }
@@ -46,24 +31,15 @@ namespace ClientApplication.Controllers
         [HttpPost]
         public string Post([FromBody] RequestData data)
         {
-            var clientConfig = new ClientConfig();
-            clientConfig
-                .GetNetworkConfig()
-                .AddAddress("0.0.0.0:5705"
-                , "0.0.0.0:5706");
-            //.AddAddress("0.0.0.0:5705");
-            clientConfig.SetGroupConfig(new GroupConfig("hc-farm", "s3crEt"));
-
             //client with custom configuration
-            Console.WriteLine(DateTime.Now.ToString() + ": POST > Connecting Hazelcast");
-            IHazelcastInstance client = HazelcastClient.NewHazelcastClient(clientConfig);
-            Console.WriteLine(DateTime.Now.ToString() + ": POST > Connected " + client.GetName());
+            IHazelcastInstance client = CacheClient.Instance;
+            Console.WriteLine(DateTime.Now + ": POST > Connected " + client.GetName());
 
             var map = client.GetMap<string, string>("mymap");
 
-            Console.WriteLine(DateTime.Now.ToString() + ": POST > Starting to set value");
+            Console.WriteLine(DateTime.Now + ": POST > Starting to set value");
             map.Put(data.Key, data.Value);
-            Console.WriteLine(DateTime.Now.ToString() + ": POST > Set value");
+            Console.WriteLine(DateTime.Now + ": POST > Set value");
 
             return "ok";
         }
